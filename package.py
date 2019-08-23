a0b849e61182f291902ffd513374c0b0ae16da84
Daniel Flood <daniel.flood-1@uts.edu.au>
2019-08-24 00:38:35 +1000

    build for changes to turret_lib-1.1.11

--- package.py
+++ package.py
@@ -2,14 +2,14 @@
 
 name = 'turret_usd'
 
-version = '1.1.2'
+version = '1.1.3'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
             'wen.tan' ]
 
 requires = [
-    'turret_lib-1+<2',
+    'turret_lib-1.1.11+<2',
     'PyOpenGL-3.1.0',
     'jemalloc-4',
     'jinja',
