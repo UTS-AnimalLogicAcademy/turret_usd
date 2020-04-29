0b6bc750f4f527814b7d8e41ec1d947004925d61
Daniel Flood <daniel.flood-1@uts.edu.au>
2020-04-29 15:23:44 +1000

    fix typo

--- package.py
+++ package.py
@@ -30,7 +30,7 @@ variants = [
 	['platform-linux', 'arch-x86_64', 'usd-20.02'],
 	['platform-linux', 'arch-x86_64', 'usd_houdini_hdk', 'houdini-17'],
 	['platform-linux', 'arch-x86_64', 'usd_houdini_hdk', 'houdini-18.0'],
-	['platform-linux', 'arch-x86_64', 'houdini-18.0']
+	['platform-linux', 'arch-x86_64', 'houdini-18.0'],
 	['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.2', 'katana-3.5.2']
 ]
 
