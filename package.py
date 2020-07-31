056e7d9f26084f4136a0651d9ab7c8ba17fe1ab0
Naomi Que <149199@ocelot.ala.int.uts.edu.au>
2020-07-31 16:47:21 +1000

    Build variant for Katana 3.5 added

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '1.1.10'
+version = '1.1.8'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -20,18 +20,21 @@ private_build_requires = [
 ]
 
 variants = [
-	['platform-linux', 'arch-x86_64', 'usd-0.8.5', 'tbb-4'],
-	['platform-linux', 'arch-x86_64', 'usd-18.09', 'tbb-4'],
-	['platform-linux', 'arch-x86_64', 'usd-18.11', 'tbb-4'],
-	['platform-linux', 'arch-x86_64', 'usd-19.01', 'tbb-4'],
-	['platform-linux', 'arch-x86_64', 'usd-19.07', 'tbb-4'],
-	['platform-linux', 'arch-x86_64', 'usd-19.11', 'tbb-4'],
-	['platform-linux', 'arch-x86_64', 'usd-20.02', 'tbb-4'],
-	['platform-linux', 'arch-x86_64', 'usd_houdini_hdk', 'houdini-17', 'tbb-4'],
-    ['platform-linux', 'arch-x86_64', 'houdini-18.0', 'tbb-2019'],
-    # ['platform-linux', 'arch-x86_64', 'houdini-18.5', 'tbb-2019'],
-	['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.2', 'katana-3.5.2', 'tbb-4']
+	['platform-linux', 'arch-x86_64', 'usd-0.8.5'],
+	['platform-linux', 'arch-x86_64', 'usd-18.09'],
+	['platform-linux', 'arch-x86_64', 'usd-18.11'],
+	['platform-linux', 'arch-x86_64', 'usd-19.01'],
+	['platform-linux', 'arch-x86_64', 'usd-19.07'],
+	['platform-linux', 'arch-x86_64', 'usd-19.11'],
+	['platform-linux', 'arch-x86_64', 'usd-20.02'],
+	['platform-linux', 'arch-x86_64', 'usd_houdini_hdk', 'houdini-17'],
+	['platform-linux', 'arch-x86_64', 'usd_houdini_hdk', 'houdini-18'],
+	['platform-linux', 'arch-x86_64', 'houdini-18'],
+	['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.3', 'katana-3.5']
 ]
 
 def commands():
+    env.KATANA_RESOURCES.append('{root}/Resources')
     env.PXR_PLUGINPATH_NAME.append('{root}/plugin/usd/')
+
+
