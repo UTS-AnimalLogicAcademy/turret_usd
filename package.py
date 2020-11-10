2002496db65fdee9c7834209a121a51486e6af15
daniel flood <daniel.flood-1@uts.edu.au>
2020-11-11 09:16:15 +1100

    removed extraneous build variants

--- package.py
+++ package.py
@@ -27,10 +27,9 @@ variants = [
 	['platform-linux', 'arch-x86_64', 'usd-19.07'],
 	['platform-linux', 'arch-x86_64', 'usd-19.11'],
 	['platform-linux', 'arch-x86_64', 'usd-20.02'],
-	['platform-linux', 'arch-x86_64', 'usd_houdini_hdk', 'houdini-17'],
-	['platform-linux', 'arch-x86_64', 'usd_houdini_hdk', 'houdini-18'],
-	['platform-linux', 'arch-x86_64', 'houdini-18'],
-	# ['platform-linux', 'arch-x86_64', 'nuke-12.2.1'],
+	['platform-linux', 'arch-x86_64', 'houdini-18.0'],
+    ##['platform-linux', 'arch-x86_64', 'houdini-18.5'],
+	## ['platform-linux', 'arch-x86_64', 'nuke-12.2.1'],
     ['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.3.1', 'katana-3.6.2', 'tbb_katana']
 ]
 
