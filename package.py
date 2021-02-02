e0609b3566b90fa9cadaa3137e9606a3ddae50f2
daniel flood <daniel.flood-1@uts.edu.au>
2021-02-03 10:57:04 +1100

    wip commit

--- package.py
+++ package.py
@@ -16,26 +16,25 @@ requires = [
 ]
 
 private_build_requires = [
-    'cmake-3.2',
+    'cmake-3.16',
 ]
 
 variants = [
-	['platform-linux', 'arch-x86_64', 'usd-0.8.5'],
-	['platform-linux', 'arch-x86_64', 'usd-18.09'],
-	['platform-linux', 'arch-x86_64', 'usd-18.11'],
-	['platform-linux', 'arch-x86_64', 'usd-19.01'],
-	['platform-linux', 'arch-x86_64', 'usd-19.07'],
-	['platform-linux', 'arch-x86_64', 'usd-19.11'],
-	['platform-linux', 'arch-x86_64', 'usd-20.02'],
-	['platform-linux', 'arch-x86_64', 'houdini-18.0'],
-    ##['platform-linux', 'arch-x86_64', 'houdini-18.5'],
+	# ['platform-linux', 'arch-x86_64', 'usd-0.8.5'],
+	# ['platform-linux', 'arch-x86_64', 'usd-18.09'],
+	# ['platform-linux', 'arch-x86_64', 'usd-18.11'],
+	# ['platform-linux', 'arch-x86_64', 'usd-19.01'],
+	# ['platform-linux', 'arch-x86_64', 'usd-19.07'],
+	# ['platform-linux', 'arch-x86_64', 'usd-19.11'],
+	# ['platform-linux', 'arch-x86_64', 'usd-20.02'],
+	# ['platform-linux', 'arch-x86_64', 'houdini-18.0'],
+    ['platform-linux', 'arch-x86_64', 'houdini-18.5.351', 'devtoolset-9']
 	## ['platform-linux', 'arch-x86_64', 'nuke-12.2.1'],
-    ['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.3.1', 'katana-3.6.2', 'tbb_katana']
+    # ['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.3.1', 'katana-3.6.2', 'tbb_katana']
+    #['platform-linux', 'arch-x86_64', 'multiverse-6.6']
 ]
 
 def commands():
     env.KATANA_RESOURCES.append('{root}/plugin')
     env.PXR_PLUGINPATH_NAME.append('{root}/plugin/usd/')
-
-
-
+    
