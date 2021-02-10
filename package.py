004b639aefb8bbccf3c8afc12f0f208e43243c21
daniel flood <daniel.flood-1@uts.edu.au>
2021-02-10 16:55:57 +1100

    updating to tbbb-2019 for houdini-18.5 variants as per vfx ref platform 2020

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '1.2.1'
+version = '1.2.2'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -27,8 +27,7 @@ variants = [
     ['platform-linux', 'arch-x86_64', 'usd-20.05', 'tbb-2017', 'boost-1.61'],
     ['platform-linux', 'arch-x86_64', 'usd-20.08', 'tbb-2017', 'boost-1.61'],
     ['platform-linux', 'arch-x86_64', 'houdini-18.0', 'devtoolset-6', 'tbb-4.4', 'boost-1.55'],
-    ['platform-linux', 'arch-x86_64', 'houdini-18.5.351', 'devtoolset-6', 'boost-1.61', 'tbb-2017'],
-    ['platform-linux', 'arch-x86_64', 'houdini-18.5.462', 'devtoolset-6', 'boost-1.61', 'tbb-2017'],
+    ['platform-linux', 'arch-x86_64', 'houdini-18.5.462', 'devtoolset-6', 'boost-1.61', 'tbb-2019'],
    # ['platform-linux', 'arch-x86_64', 'nuke-12.2.1', '!katana'],
     ['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.3.1', 'katana-3.6.2', 'tbb_katana']
 ]
