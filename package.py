259fa87fb45d3f216db3495e1c9bbd11cd3cb181
Jonah Newton <jonah.newton@uts.edu.au>
2022-05-11 11:39:32 +1000

    Added variant for USD-21.05

--- package.py
+++ package.py
@@ -2,11 +2,12 @@
 
 name = 'turret_usd'
 
-version = '1.2.7'
+version = '1.2.9'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
-            'wen.tan' ]
+            'wen.tan',
+	    'jonah.newton' ]
 
 requires = [
     'turret_lib-1.2+<2',
@@ -24,6 +25,7 @@ variants = [
     ['platform-linux', 'arch-x86_64', 'usd-19.11', 'tbb-2017.0', 'boost-1.55', '!katana'], # maya 2019
     ['platform-linux', 'arch-x86_64', 'houdini-18.0', 'devtoolset-6', 'boost-1.61', 'tbb-2019.0', '!katana'], # usd-19.11
     ['platform-linux', 'arch-x86_64', 'KatanaUsdPlugins-19.11.3.1', 'katana-3.6.2', 'tbb_katana'], # usd-19.11
+    ['platform-linux', 'arch-x86_64', 'usd-21.05', 'katana-4.5.2', 'tbb_katana-2019.6'], # usd-21.05
     # ['platform-linux', 'arch-x86_64', 'nuke-12.2.1', '!katana'], # usd-19.11
 
     ['platform-linux', 'arch-x86_64', 'usd-20.08', 'tbb-2017', 'boost-1.61', '!katana'],
