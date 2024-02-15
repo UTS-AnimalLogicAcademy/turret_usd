a6b7423f4a3ed9bb8a1fdf682a0540947ad39f8a
Jonah Newton <jonah.newton@uts.edu.au>
2024-02-15 14:28:17 +1100

    Katana 6 variant

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '2.0.2'
+version = '2.0.3'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -23,7 +23,8 @@ private_build_requires = [
 variants = [
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd-23.08', '!katana', '!nuke', '!maya', '!houdini'], #usd-23.08 standalone
     ['platform-linux', 'arch-x86_64', 'jinja-2', 'tbb-2020.3', 'boost-1.72', 'python-3.7', 'houdini-19.5.640', '!katana', '!nuke', '!maya'], #houdini 19.5.640
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.590', '!katana', '!nuke', '!maya'] #houdini 20.0.590
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.590', '!katana', '!nuke', '!maya'], #houdini 20.0.590
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'python-3.9', 'katana-6', '!houdini', '!nuke', '!maya'] #Katana 6
 
 ]
 
