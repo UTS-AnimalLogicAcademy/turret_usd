3dd0798e95af80286926de67b730ecd16ee0519c
Jonah Newton <jonah.newton@uts.edu.au>
2024-04-05 12:35:25 +1100

    usd_monolithic and usd do not clash anymore.

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '2.0.6'
+version = '2.1.0'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -10,7 +10,7 @@ authors = [ 'ben.skinner',
 	    'jonah.newton' ]
 
 requires = [
-    'turret_lib-1.2+<2',
+    'turret_lib-1.4+<2',
     'PyOpenGL-3.1.0',
     'jemalloc-4',
 ]
@@ -21,8 +21,8 @@ private_build_requires = [
 ]
 
 variants = [
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd-23.08', '!katana', '!nuke', '!maya', '!houdini', '!blender'], #usd-23.08 standalone
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd_monolithic-23.08', '!houdini', '!katana', '!maya', '!nuke'], #USD Monolithic 23.08 / Blender 3.6
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd-23.08', '!usd_monolithic', '!katana', '!nuke', '!maya', '!houdini', '!blender'], #usd-23.08 standalone
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd_monolithic-23.08', '!usd', '!houdini', '!katana', '!maya', '!nuke'], #USD Monolithic 23.08 / Blender 3.6
     ['platform-linux', 'arch-x86_64', 'jinja-2', 'tbb-2020.3', 'boost-1.72', 'python-3.7', 'houdini-19.5.640', '!katana', '!nuke', '!maya', '!blender'], #houdini 19.5.640
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.590', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.0.590
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'python-3.9', 'katana-6', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6
