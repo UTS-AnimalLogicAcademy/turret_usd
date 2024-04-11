0ddb0740dd385b2fbab7a449ab62da3f5cee7190
Jonah Newton <jonah.newton@uts.edu.au>
2024-04-11 16:38:43 +1000

    Up Houdini versions

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '2.1.0'
+version = '2.2.0'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -23,8 +23,8 @@ private_build_requires = [
 variants = [
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd-23.08', '!usd_monolithic', '!katana', '!nuke', '!maya', '!houdini', '!blender'], #usd-23.08 standalone
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd_monolithic-23.08', '!usd', '!houdini', '!katana', '!maya', '!nuke'], #USD Monolithic 23.08 / Blender 3.6
-    ['platform-linux', 'arch-x86_64', 'jinja-2', 'tbb-2020.3', 'boost-1.72', 'python-3.7', 'houdini-19.5.640', '!katana', '!nuke', '!maya', '!blender'], #houdini 19.5.640
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.590', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.0.590
+    ['platform-linux', 'arch-x86_64', 'jinja-2', 'tbb-2020.3', 'boost-1.72', 'python-3.9', 'houdini-19.5.805', '!katana', '!nuke', '!maya', '!blender'], #houdini 19.5.640
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.653', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.0.590
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'python-3.9', 'katana-6', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.76', 'python-3.9', 'nuke-14.1', '!houdini', '!katana', '!maya', '!blender'], #Nuke 14.1
     
