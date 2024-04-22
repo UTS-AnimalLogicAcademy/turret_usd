8933d6174c98a0e773ee3c025edc38bc4e9ac1cc
Jonah Newton <jonah.newton@uts.edu.au>
2024-04-22 12:52:40 +1000

    Add Katana 6.5 variant

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '2.3.0'
+version = '2.4.0'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -26,7 +26,8 @@ variants = [
     ['platform-linux', 'arch-x86_64', 'jinja-2', 'tbb-2020.3', 'boost-1.72', 'python-3.9', 'houdini-19.5.805', '!katana', '!nuke', '!maya', '!blender'], #houdini 19.5.640
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.653', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.0.590
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82', 'python-3.10', 'houdini-20.5.207', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.5.207 py3.10
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'python-3.9', 'katana-6', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'python-3.9', 'katana-6.0', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'python-3.9', 'katana-6.5', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6.5
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.76', 'python-3.9', 'nuke-14.1', '!houdini', '!katana', '!maya', '!blender'], #Nuke 14.1
     
 
