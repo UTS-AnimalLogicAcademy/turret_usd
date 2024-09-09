7291f7e7d1566558bc5d917923b4bae4bf6bc557
Jonah Newton <jonah.newton@uts.edu.au>
2024-09-09 15:39:55 +1000

    Add Mari 7 / Katana 7 variants

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '2.5.1'
+version = '2.6.0'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -28,10 +28,12 @@ variants = [
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.751', '!mari', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.0.590
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82', 'python-3.10', 'houdini-20.5.207', '!mari', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.5.207 py3.10
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76','jemalloc-4', 'python-3.9', 'katana-6.0', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.80', 'jemalloc-4', 'python-3.10', 'katana-7.0', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 7
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76','jemalloc-4', 'python-3.9', 'katana-6.5', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6.5
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.76','jemalloc-4', 'python-3.9', 'nuke-14.1', '!mari', '!houdini', '!katana', '!maya', '!blender'], #Nuke 14.1
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80','jemalloc-4', 'python-3.10', 'nuke-15.1', '!mari', '!houdini', '!katana', '!maya', '!blender','!usd'], #Nuke 15.1
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'mari-6','jemalloc-4', 'python-3.9', '!nuke', '!houdini', '!katana', '!maya', '!blender'], #Mari 6
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.80', 'mari-7','jemalloc-4', 'python-3.10', '!nuke', '!houdini', '!katana', '!maya', '!blender'], #Mari 7
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82', 'maya-2025','jemalloc-4', 'python-3.11', '!nuke', '!houdini', '!katana', '!mari', '!blender'], #Maya 2025
     
 
