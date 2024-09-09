17074abae566d2f7929393a5eb36ebe07148f6d1
Jonah Newton <jonah.newton@uts.edu.au>
2024-09-09 10:49:26 +1000

    Rebuild for non-CXX11 ABI boost-1.80

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '2.5.0'
+version = '2.5.1'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -30,7 +30,7 @@ variants = [
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76','jemalloc-4', 'python-3.9', 'katana-6.0', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76','jemalloc-4', 'python-3.9', 'katana-6.5', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6.5
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.76','jemalloc-4', 'python-3.9', 'nuke-14.1', '!mari', '!houdini', '!katana', '!maya', '!blender'], #Nuke 14.1
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80','jemalloc-4', 'python-3.10', 'nuke-15.1', '!mari', '!houdini', '!katana', '!maya', '!blender'], #Nuke 15.1
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80','jemalloc-4', 'python-3.10', 'nuke-15.1', '!mari', '!houdini', '!katana', '!maya', '!blender','!usd'], #Nuke 15.1
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'mari-6','jemalloc-4', 'python-3.9', '!nuke', '!houdini', '!katana', '!maya', '!blender'], #Mari 6
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82', 'maya-2025','jemalloc-4', 'python-3.11', '!nuke', '!houdini', '!katana', '!mari', '!blender'], #Maya 2025
     
