f82b2ecd38dc7230c306b47727e848ced5ef6486
Jonah Newton <jonah.newton@uts.edu.au>
2025-01-23 12:51:08 +1100

    Specify boost-1.82 variants + use HDK Jemalloc for non-monolithic

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_usd'
 
-version = '2.6.3'
+version = '2.6.4'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -23,10 +23,10 @@ private_build_requires = [
 
 variants = [
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80', 'usd-24.08','jemalloc-4', '!usd_monolithic', '!mari', '!katana', '!nuke', '!maya', '!houdini', '!blender'], #usd-23.08 standalone
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82', 'usd_monolithic_blender-24.05','jemalloc-4', '!usd', '!mari', '!houdini', '!katana', '!maya', '!nuke'], #Blender 4.2
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82.0.1', 'usd_monolithic_blender-24.05','jemalloc-4', '!usd', '!mari', '!houdini', '!katana', '!maya', '!nuke'], #Blender 4.2
     ['platform-linux', 'arch-x86_64', 'jinja-2', 'tbb-2020.3', 'boost-1.72', 'python-3.9', 'houdini-19.5.805', '!mari', '!katana', '!nuke', '!maya', '!blender'], #houdini 19.5.640
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.72', 'python-3.10', 'houdini-20.0.751', '!mari', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.0.590
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82', 'python-3.10', 'houdini-20.5.445', '!mari', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.5.445 py3.10
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82.0.0', 'python-3.10', 'houdini-20.5.445', '!mari', '!katana', '!nuke', '!maya', '!blender'], #houdini 20.5.445 py3.10
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76','jemalloc-4', 'python-3.9', 'katana-6.0', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 6
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.80', 'jemalloc-4', 'python-3.10', 'katana-7.0', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 7
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.82', 'jemalloc-4', 'python-3.11', 'katana-8.0', '!mari', '!houdini', '!nuke', '!maya', '!blender'], #Katana 8
@@ -35,7 +35,7 @@ variants = [
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.80','jemalloc-4', 'python-3.10', 'nuke-15.1', '!mari', '!houdini', '!katana', '!maya', '!blender','!usd'], #Nuke 15.1
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.76', 'mari-6','jemalloc-4', 'python-3.9', '!nuke', '!houdini', '!katana', '!maya', '!blender'], #Mari 6
     ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb_katana-2020.3', 'boost_katana-1.80', 'mari-7','jemalloc-4', 'python-3.10', '!nuke', '!houdini', '!katana', '!maya', '!blender'], #Mari 7
-    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82', 'maya-2025','jemalloc-4', 'python-3.11', '!nuke', '!houdini', '!katana', '!mari', '!blender'], #Maya 2025
+    ['platform-linux', 'arch-x86_64', 'jinja-3', 'tbb-2020.3', 'boost-1.82.0.1', 'maya-2025','jemalloc-4', 'python-3.11', '!nuke', '!houdini', '!katana', '!mari', '!blender'], #Maya 2025
     
 
 ]
